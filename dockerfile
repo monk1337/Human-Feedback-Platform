4af840ac32f616dcc75783da7b7db9ca0469257c
BLooDBRothER <computertechnology446@gmail.com>
2024-01-15 17:45:02 +0530

    Add docker and docker compose for fastapi

---
+++ dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11.1-slim
+
+RUN mkdir /app
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY . .
--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from src.main import app
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
